a78783a074b36d2894bbc07a0e5a218d7b3cd81a
Rekha1912 <rekha.0019s@gmail.com>
2020-09-05 18:13:33 -0500

    added day view

--- src/components/CurrentDate.jsx
+++ src/components/CurrentDate.jsx
@@ -4,22 +4,31 @@ import { render } from "react-dom";
 class CurrentDate extends Component {
   constructor(props){
     super(props);
-    var today = new Date();
-      //var dd = String(today.getDate()).padStart(2, '0');
-      //var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-      //var yyyy = today.getFullYear();
 
-      var dateToday = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
-      this.state={dateToday: dateToday};
+      var today = new Date();
+        var weekday = new Array(7);
+          weekday[0] = "Sunday";
+          weekday[1] = "Monday";
+          weekday[2] = "Tuesday";
+          weekday[3] = "Wednesday";
+          weekday[4] = "Thursday";
+          weekday[5] = "Friday";
+          weekday[6] = "Saturday";
+
+        var dayToday = weekday[today.getDay()];
+        var dateToday = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+
+    this.state={dayToday: dayToday, dateToday: dateToday};
   }
 
   render() {
     return (
       <div>
+        {this.state.dayToday}  < br/>
         {this.state.dateToday}
       </div>
     );
   }
-
   };
 export default CurrentDate;
+
